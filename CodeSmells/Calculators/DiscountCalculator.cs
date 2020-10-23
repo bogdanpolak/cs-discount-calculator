c0f792ac90fcde3e581bd4d37448bae1de04f608
Bogdan Polak <bogdanpol@gmail.com>
2020-10-23 22:57:28 +0200

    Implemented calculator

---
+++ CodeSmells/Calculators/DiscountCalculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeSmells.Calculators
+{
+    public class DiscountCalculator
+    {
+        private class Discount
+        {
+            public decimal RangeStart { get; }
+            public decimal RangeEnd { get; }
+            public decimal Value { get;  }
+
+            public Discount(decimal rangeStart, decimal rangeEnd, decimal value)
+            {
+                RangeStart = rangeStart;
+                RangeEnd = rangeEnd;
+                Value = value;
+            }
+        }
+
+        private Dictionary<decimal, double> thresholds = new Dictionary<decimal, double>()
+        {
+            { 1000.00m, 0.03d },
+            { 1400.00m, 0.05d },
+            { 1900.00m, 0.08d }
+        };
+
+        const decimal MAX_PRICE = 999999999.00m;
+
+        private List<Discount> discounts = new List<Discount>();
+
+        private void GenerateDiscountTable()
+        {
+            var rangeStart = 0m;
+            var discount = 0m;
+            foreach (var pair in thresholds)
+            {
+                discounts.Add(new Discount(rangeStart, pair.Key, discount));
+                discount = (decimal)pair.Value;
+                rangeStart = pair.Key;
+            }
+            discounts.Add(new Discount(rangeStart, MAX_PRICE, discount));
+        }
+
+        public Decimal Calculate(IEnumerable<OrderItem> orderItems)
+        {
+            var totalBeforeDiscount = orderItems.Sum(oi => oi.UnitPrice * oi.Units);
+            if (discounts.Count == 0)
+                GenerateDiscountTable();
+            var discountValue = discounts
+                .Where(d => totalBeforeDiscount.InRange(d.RangeStart, d.RangeEnd))
+                .Select(d => d.Value)
+                .FirstOrDefault();
+            var totalAfterDeduction = 0m;
+            foreach (OrderItem item in orderItems)
+            {
+                if (discountValue>0 && item.AllowsDeduction)
+                {
+                    item.Deduction = discountValue;
+                    item.Total = item.UnitPrice * item.Units * (1-item.Deduction);
+                }
+                else
+                {
+                    item.Total = item.UnitPrice * item.Units;
+                }
+                totalAfterDeduction += item.Total;
+            }
+            return totalAfterDeduction;
+        }
+    }
+}
--- CodeSmells/Calculators/OrderItem.cs
+++ CodeSmells/Calculators/OrderItem.cs
@@ -1,10 +1,25 @@
-﻿using System;
-namespace CodeSmells.Calculators
+﻿namespace CodeSmells.Calculators
 {
-    public class OrderItems
+    public class OrderItem
     {
-        public OrderItems()
+        public int ProductId { get; }
+        public string ProductName { get; }
+        public bool AllowsDeduction { get; set; }
+        public decimal UnitPrice { get; }
+        public int Units { get; set; }
+        public decimal Deduction { get; set; }
+        public decimal Total { get; set; }
+
+        public OrderItem(int productId, string productName,
+            bool allowsDeduction, decimal unitPrice, int units)
         {
+            ProductId = productId;
+            ProductName = productName;
+            AllowsDeduction = allowsDeduction;
+            UnitPrice = unitPrice;
+            Units = units;
+            Deduction = 0;
+            Total = unitPrice * units;
         }
     }
 }
