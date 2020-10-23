cf7ad6be0d62926d872f3e13072e04550d3be76d
Bogdan Polak <bogdanpol@gmail.com>
2020-10-24 00:43:28 +0200

    Added CustomerLevel

--- CodeSmells/TestDiscountCalculator.cs
+++ CodeSmells/TestDiscountCalculator.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Xunit;
 using CodeSmells.Calculators;
+using CodeSmells.Repositories;
 
 namespace CodeSmells
 {
@@ -9,20 +10,20 @@ namespace CodeSmells
         [Fact]
         public void Calculate_WithOneItem_WhenTotalBelowDiscount()
         {
-            var order = new List<OrderItem>() {
+            var orders = new List<OrderItem>() {
                 new OrderItem(25, "blue jeans trousers", true, 100, 2)
             };
-            var actual = new DiscountCalculator().Calculate(order);
+            var actual = new DiscountCalculator().Calculate(CustomerLevel.gold, orders);
             Assert.Equal(200.00m, actual);
         }
 
         [Fact]
         public void Calculate_WithOneItem_WhenTotalIsAboveFirstTreshold()
         {
-            var order = new List<OrderItem>() {
+            var orders = new List<OrderItem>() {
                 new OrderItem(25, "blue jeans trousers", true, 100, 11)
             };
-            var actual = new DiscountCalculator().Calculate(order);
+            var actual = new DiscountCalculator().Calculate(CustomerLevel.gold, orders);
             // 1100 * 0.3 = 1067
             Assert.Equal(1067.00m, actual);
         }
@@ -30,7 +31,7 @@ namespace CodeSmells
         [Fact]
         public void Calculate_WithDiscount_OneItemNotDeducted()
         {
-            var order = new List<OrderItem>() {
+            var orders = new List<OrderItem>() {
                 new OrderItem(25, "blue jeans trousers", true, 100, 20),
                 new OrderItem(2, "transport service", false, 90, 1),
                 new OrderItem(99, "sport shoes", true, 120, 4)
@@ -38,7 +39,7 @@ namespace CodeSmells
             // total before: 2570 = 2000 + 90 + 480
             // discount: 8%
             // 2000*0.92 + 90 + 480*0.92 = 1840 + 90 + 441,60 = 2371.60
-            var actual = new DiscountCalculator().Calculate(order);
+            var actual = new DiscountCalculator().Calculate(CustomerLevel.gold, orders);
             Assert.Equal(2371.60m, actual);
         }
     }
