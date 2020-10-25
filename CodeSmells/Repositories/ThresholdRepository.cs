ece2509c475872c966737c6e90e7ec4d3944efaa
Bogdan Polak <bogdanpol@gmail.com>
2020-10-25 09:46:14 +0100

    Extracted Threshold class to separate unit

--- CodeSmells/Repositories/ThresholdRepository.cs
+++ CodeSmells/Repositories/ThresholdRepository.cs
@@ -4,26 +4,6 @@ using System.Linq;
 
 namespace CodeSmells.Repositories
 {
-    public class Treshold {
-        public string Level { get; }
-        public decimal LimitBottom { get; }
-        public double Discount { get; }
-
-        public static bool isLevelValid (string level) => 
-            (level == "standard" || level == "silver" || level == "gold");
-
-        public Treshold(string level, decimal limitBottom, double discount)
-        {
-            if ( !isLevelValid(level) )
-                throw new ArgumentException(
-                    $"Invalid customer level (actual value:{level}"+
-                    ", but expected one of: standard, silver, gold", "level");
-            Level = level;
-            LimitBottom = limitBottom;
-            Discount = discount;
-        }
-    }
-
     public class TresholdRepository {
         private List<Treshold> thresholds = new List<Treshold>()
         {
---
+++ CodeSmells/Repositories/Treshold.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CodeSmells.Repositories
+{
+    public class Treshold
+    {
+        public string Level { get; }
+        public decimal LimitBottom { get; }
+        public double Discount { get; }
+
+        public static bool isLevelValid(string level) =>
+            (level == "standard" || level == "silver" || level == "gold");
+
+        public Treshold(string level, decimal limitBottom, double discount)
+        {
+            if (!isLevelValid(level))
+                throw new ArgumentException(
+                    $"Invalid customer level (actual value:{level}" +
+                    ", but expected one of: standard, silver, gold", "level");
+            Level = level;
+            LimitBottom = limitBottom;
+            Discount = discount;
+        }
+    }
+
+}
